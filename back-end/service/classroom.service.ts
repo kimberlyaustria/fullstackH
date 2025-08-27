5cdae936f828c09de8b53dc3c46614635138c6e3
kimberlyaustria <kimberly.austria@student.ucll.be>
2025-08-27 16:10:53 +0200

    cleanup

--- back-end/service/classroom.service.ts
+++ back-end/service/classroom.service.ts
@@ -15,27 +15,6 @@ const createClassroom = async ({ name }: ClassroomInput): Promise<Classroom> => 
     return await classroomDb.createClassroom(classroom);
 };
 
-// const createClassroom = async ({
-//     name,
-//     role,
-// }: ClassroomInput & { role: string }): Promise<Classroom> => {
-//     if (role !== 'admin') {
-//         throw new UnauthorizedError('credentials_required', {
-//             message: 'You are not authorized to access this resource.',
-//         });
-//     }
-
-//     const existingClassroom = await classroomDb.getClassroomByName(name);
-
-//     if (existingClassroom) {
-//         throw new Error('Classroom with name ${name} already exists.');
-//     }
-
-//     const classroom = new Classroom({ name });
-
-//     return await classroomDb.createClassroom(classroom);
-// };
-
 export default {
     createClassroom,
 };
